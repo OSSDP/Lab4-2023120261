d0ec76e9f2d8d162f13c1757d38f74b6b5c9e6bd
Xxxxil <1071501942@qq.com>
2024-12-15 10:27:02 +0800

    update

--- src/test/java/L2023120261_18_Test.java
+++ src/test/java/L2023120261_18_Test.java
@@ -1,5 +1,5 @@
-package org.example;
 //2023120244 验证测试通过
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
